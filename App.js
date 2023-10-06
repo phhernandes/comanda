6eb5fb45effa56db9acf6d469b33f4794441e25c
phhernandes <pedrobeco97@gmail.com>
2023-10-06 09:35:39 -0300

    commit

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TextComponent, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image  style={styles.imagem} source={require('./assets/buerger.jpg')}/>
+      <Text>Sistema</Text>
+      <Text style={styles.titulo}>Comanda</Text>
+      <TextInput style={styles.input} placeholder='e-mail' />
+      <TextInput style={styles.input} placeholder='senha' secureTextEntry />
+      <Text style={styles.botao}>entrar</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FCFBFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    color: '#121A2C',
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  input: {
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    borderRadius: 4,
+    borderColor: "#747474",
+    borderWidth: 1,
+    marginVertical: 10,
+  },
+  botao:{
+    color:'#ffba26',
+    backgroundColor: '#121a2c',
+    paddingVertical: 12,
+    paddingHorizontal: 40,
+    borderRadius: 8,
+  },
+  imagem:{
+    width: '100%',
+    height: 200,
+    position: "absolute",
+    top: 0,
+  }
 });
